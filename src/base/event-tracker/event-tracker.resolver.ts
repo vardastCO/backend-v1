c64f8428ad4295bd161904ddee35f770f3318b87
partovi <partovi74@gmail.com>
2024-02-20 15:43:25 +0330

    change middleware

--- src/base/event-tracker/event-tracker.resolver.ts
+++ src/base/event-tracker/event-tracker.resolver.ts
@@ -14,7 +14,7 @@ export class EventTrackerResolver {
   constructor(private readonly eventTrackerService: EventTrackerService) {}
 
   @Public()
-  @Mutation(() => EventTracker)
+  @Mutation(() => Boolean)
     
   @Permission("gql.base.event_tracker.create")
   createEventTracker(
