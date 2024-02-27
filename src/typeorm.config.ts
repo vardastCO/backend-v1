81fd178448a624c69c918f4ed36f914a240c993b
farbodnp74 <91656406+farbodnp74@users.noreply.github.com>
2024-02-27 20:33:36 +0330

    add gtag by farbod

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -14,4 +14,9 @@ export default new DataSource({
   database: configService.get("DB_NAME"),
   entities: ["dist/**/*.entity.js"],
   migrations: ["dist/src/migrations/*.js"],
+  pool: {
+    max: 10, // Maximum number of connections in the pool
+    min: 2,  // Minimum number of connections in the pool
+    acquireTimeoutMillis: 30000, // The maximum time, in milliseconds, that the pool will try to get connection before throwing an error
+  },
 });
