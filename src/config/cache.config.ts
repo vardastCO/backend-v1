5974da45c7f1a033dffeb1cc1a68a9989fd5942e
partovi <partovi74@gmail.com>
2024-02-28 11:30:55 +0330

    change middleware

--- src/config/cache.config.ts
+++ src/config/cache.config.ts
@@ -1,8 +1,10 @@
 import {
   CacheModuleAsyncOptions,
   CacheModuleOptions,
+  CacheStore,
 } from "@nestjs/cache-manager";
 import { ConfigModule, ConfigService } from "@nestjs/config";
+import { redisStore } from "cache-manager-redis-store";
 import { CacheTTL } from "src/base/utilities/cache-ttl.util";
 
 export const cacheAsyncConfig: CacheModuleAsyncOptions = {
@@ -12,6 +14,7 @@ export const cacheAsyncConfig: CacheModuleAsyncOptions = {
   useFactory: async (
     configService: ConfigService,
   ): Promise<CacheModuleOptions> => ({
+    store: redisStore as unknown as CacheStore,
     url: `redis://${configService.get("REDIS_HOST")}:${configService.get(
       "REDIS_PORT",
     )}`,
