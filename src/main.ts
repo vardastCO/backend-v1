77e54a92039e923a13b31d6cff1e3d5ef47a0785
partovi <partovi74@gmail.com>
2024-03-03 10:44:20 +0330

    change middleware

--- src/main.ts
+++ src/main.ts
@@ -43,7 +43,14 @@ async function bootstrap() {
   // await initializeApm()
 
   app.enableCors();
-  app.use(compression());
+  app.use(
+    compression({
+      level: 9,       // Maximum compression level
+      memLevel: 9,    // Maximum memory usage for compression
+      strategy: compression.constants.Z_HUFFMAN_ONLY, // Use Huffman coding only for maximum compression
+      // Add more compression options as needed
+    })
+  );
   app.use(new TimingMiddleware().use);
   // app.useLogger(logger);
   // try {
