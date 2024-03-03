05299f8ae476a15e2b3e8ca86f59f74d9fffbc43
partovi <partovi74@gmail.com>
2024-03-03 16:52:43 +0330

    change middleware

--- src/products/brand/entities/brand.entity.ts
+++ src/products/brand/entities/brand.entity.ts
@@ -3,8 +3,6 @@ import { File } from "src/base/storage/file/entities/file.entity";
 import { Category } from "src/base/taxonomy/category/entities/category.entity";
 import { ThreeStateSupervisionStatuses } from "src/base/utilities/enums/three-state-supervision-statuses.enum";
 import { Product } from "src/products/product/entities/product.entity";
-import { SellerHasBrand } from "src/products/seller/entities/seller-has-brnads.entity";
-import { Seller } from "src/products/seller/entities/seller.entity";
 import {
   BaseEntity,
   Column,
@@ -89,10 +87,7 @@ export class Brand extends BaseEntity {
   @Field(type => [Product], { nullable: "items" })
   @OneToMany(type => Product, product => product.brand)
   products: Promise<Product[]>;
-
-  @Field(() => [Seller], { nullable: "items" })
-  @OneToMany(() => SellerHasBrand, sellerHasBrand => sellerHasBrand.brand)
-  sellers: Seller[];
+  
 
   @Field(() => File, { nullable: true })
   @OneToOne(() => File, file => null, { eager: true, nullable: true })
