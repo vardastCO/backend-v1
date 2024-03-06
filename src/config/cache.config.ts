f9965799864cd249869c91fd8f8f24951df4cf94
partovi <partovi74@gmail.com>
2024-03-06 17:39:26 +0330

    change middleware

--- src/config/cache.config.ts
+++ src/config/cache.config.ts
@@ -15,7 +15,7 @@ export const cacheAsyncConfig: CacheModuleAsyncOptions = {
     configService: ConfigService,
   ): Promise<CacheModuleOptions> => ({
     store: redisStore as unknown as CacheStore,
-    url: `redis://${configService.get("REDIS_HOST")}:${configService.get("REDIS_PORT")}?password=${configService.get("REDIS_PASSWORD")}`,
+    url: `redis://${configService.get("REDIS_HOST")}:${configService.get("REDIS_PORT")}?password=g90pM89O}`,
     ttl: CacheTTL.ONE_WEEK,
     max: 30, // Adjust based on performance testing
     min: 10,  // Minimum number of connections in the pool
