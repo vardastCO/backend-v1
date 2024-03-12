c3653245b42184264da4fa1d96ec17aa29aaf2f5
partovi <partovi74@gmail.com>
2024-03-12 10:25:54 +0330

    change middleware

--- src/products/product/product.service.ts
+++ src/products/product/product.service.ts
@@ -413,6 +413,13 @@ export class ProductService {
   }
   async getPublicOffersOf(product: Product): Promise<Offer[]> {
 
+    // const cacheKey = `public_offers_${JSON.stringify(product.id)}`;
+    // const cachedData = await this.cacheManager.get<Offer[]>(cacheKey);
+  
+    // if (cachedData) {
+    //   // return cachedData;
+    // }
+
     const offers = await Offer.createQueryBuilder()
       .innerJoin(
         subQuery =>
@@ -430,10 +437,17 @@ export class ProductService {
         "maxIds",
         '"Offer"."id" = "maxIds"."maxId"',
     )
-      .take(5)
+      // .take(5)
+      // .orderBy(
+      //   { createdAt: 'DESC' }
+         
+      // )
+      // .innerJoinAndSelect('Offer."lastPublicConsumerPrice"', 'lastPublicConsumerPrice') // Removed double quotes around "Offer"
+      // .orderBy('lastPublicConsumerPrice.createdAt', 'DESC')
       .orderBy('"Offer"."createdAt"', 'DESC') 
       .getMany();
     
+    // await this.cacheManager.set(cacheKey,offers,CacheTTL.ONE_DAY)
 
     return offers;
   }
