72aa4cd4ccaea76f3c813bf101742dbc90896518
partovi <partovi74@gmail.com>
2024-04-17 13:13:21 +0330

    change slider

--- src/products/brand/brand.service.ts
+++ src/products/brand/brand.service.ts
@@ -174,9 +174,13 @@ export class BrandService {
         parsedData.updatedAt = new Date();
         const catalogFile = await parsedData.catalog;
 
+        console.log('catalogFile',catalogFile)
+
         if (catalogFile) {
           // Now you can access the properties of the resolved File object
           const catalogCreatedAt = await parsedData.createdAt;
+
+          console.log('catalogCreatedAt',catalogCreatedAt)
         
           if (catalogCreatedAt) {
             (await parsedData.catalog).createdAt = new Date(catalogCreatedAt);
