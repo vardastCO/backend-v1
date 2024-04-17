d527e28c4e5c7defd5d70a0e6861838220831f60
partovi <partovi74@gmail.com>
2024-04-17 14:25:11 +0330

    change slider

--- src/base/storage/file/dto/index-banner.input.ts
+++ src/base/storage/file/dto/index-banner.input.ts
@@ -1,8 +1,10 @@
 import { Field, InputType } from "@nestjs/graphql";
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty,IsOptional } from "class-validator";
 import { FileModelTypeEnum } from "../enums/file-model-type.enum";
 
 @InputType()
 export class IndexBannerInput {
-
+  @Field(() => FileModelTypeEnum,{nullable:true})
+  @IsOptional()
+  type?: FileModelTypeEnum;
 }
