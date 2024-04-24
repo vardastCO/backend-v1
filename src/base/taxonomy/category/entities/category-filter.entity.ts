e594bd409e1742a1493668d39c8cf4a00bdfa308
partovi <partovi74@gmail.com>
2024-04-24 09:17:04 +0330

    change

---
+++ src/base/taxonomy/category/entities/category-filter.entity.ts
@@ -0,0 +1,41 @@
+import { Field, Int, ObjectType } from "@nestjs/graphql";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Category } from "./category.entity";
+import { AttributesProductService } from "src/products/attribute/entities/attribute_product.entity";
+
+
+
+
+@ObjectType()
+@Entity("category_filter")
+export class Banner extends BaseEntity {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field(() => Category)
+  @OneToOne(() => Category, category => null, { eager: true })
+  @JoinColumn()
+  category: Promise<Category>;
+  @Index()
+  @Column()
+  categoryId: number;
+
+  @Field(() => AttributesProductService)
+  @OneToOne(() => AttributesProductService, attribuite => null, { eager: true })
+  @JoinColumn()
+  attribuite: Promise<AttributesProductService>;
+  @Index()
+  @Column()
+  attribuiteId: number;
+
+
+}
