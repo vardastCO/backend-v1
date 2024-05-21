aa1c0d36c5b0e8ebf26741374ce503882fef63e6
partovi <partovi74@gmail.com>
2024-05-21 18:23:06 +0330

    c

--- src/products/product/product-csv-update-image.command.ts
+++ src/products/product/product-csv-update-image.command.ts
@@ -58,6 +58,9 @@ export class ProductCsvUpdateImageCommand extends CommandRunner {
     this.directory = await Directory.findOneBy({ path: "product/image/files" });
     this.bucketName = this.configService.get("STORAGE_MINIO_DEFAULT_BUCKET");
   }
+  delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
   async run(passedParam: string[], options?: any): Promise<void> {
     const [csvFile, imageDirectory] = passedParam;
     const stream = Fs.createReadStream(csvFile, "utf8");
@@ -87,7 +90,7 @@ export class ProductCsvUpdateImageCommand extends CommandRunner {
         if (!product) {
           throw product;
         }
-        console.log('ppp',product)
+ 
         const productImages = await product.images;
       
         // Check if the product already has images
@@ -95,7 +98,7 @@ export class ProductCsvUpdateImageCommand extends CommandRunner {
 
         // Check if the product already has images
         if (productImages && productImages.length > 0) {
-          console.log('Skipped adding images for', product.name, 'as it already has images.');
+          // console.log('Skipped adding images for', product.name, 'as it already has images.');
           continue; // Continue to the next product if images exist
         }
         // const productSkus = [sku];
@@ -103,38 +106,38 @@ export class ProductCsvUpdateImageCommand extends CommandRunner {
         //   `^(${productSkus.join("|")})-(\\d+).(jpg|jpeg|png|webp)$`,
         // );
         let i = 1;
-        console.log('lengh',this.files.length)
         for (const filename of this.files) {
           try {
             // Split the filename into parts
             const parts = filename.split('-');
-            const fileSku = parts[0];
+            const fileSku = parts[0].toString();
             const sortPart = parts[1];
             const extension = filename.split('.').pop().toLowerCase();
+            let skuString : string = sku.toString() 
 
-            console.log('extension',extension)
-            console.log('fileSku', fileSku)
-            console.log('sku',sku)
             // Check if the SKU matches and the extension is valid
             const validExtensions = ['jpg', 'jpeg', 'png', 'webp'];
-            if (fileSku == sku && validExtensions.includes(extension)) {
+            console.log('=========================')
+            console.log(fileSku,fileSku==skuString,skuString)
+            console.log('=========================')
+            if (fileSku == skuString) {
               const sortOrder = parseInt(sortPart, 10) || i++;
-    
+              
               // Log details for debugging
               console.log(`Processing file: ${filename}, SKU: ${fileSku}, sort order: ${sortOrder}, extension: ${extension}`);
     
               // Add the image to the product
               await this.addImage(imageDirectory, filename, product, sortOrder);
-    
+              await this.delay(100);
               // Remove the processed file from the list
-              this.files = this.files.filter((file) => file !== filename);
+              // this.files = this.files.filter((file) => file !== filename);
             }
           } catch (error) {
             console.log('Warning:', error);
           }
         }
   
-        console.log('Saved', product.name);
+        // console.log('Saved', product.name);
       }
     } catch (e) {
       console.log('rrrrrr', e);
