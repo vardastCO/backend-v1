777bb0114d2b7e06591757de16a3ca55c95874a1
partovi <partovi74@gmail.com>
2024-05-25 21:56:40 +0330

    c

--- src/order/preOrder/pre-order.service.ts
+++ src/order/preOrder/pre-order.service.ts
@@ -117,7 +117,7 @@ export class PreOrderService {
         relations: ["files", "lines"],
         order: {
           lines: {
-            type : 'DESC'
+            type : 'ASC'
           }
         }
       })
