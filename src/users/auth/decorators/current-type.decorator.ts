de6ded66ffa33f2a3999542cd9427af628eb1850
partovi <partovi74@gmail.com>
2024-06-04 20:34:05 +0330

    c

--- src/users/auth/decorators/current-type.decorator.ts
+++ src/users/auth/decorators/current-type.decorator.ts
@@ -15,10 +15,19 @@ export const CurrentType = createParamDecorator(
     console.log('token',token)
     try {
       console.log('process.env.AUTH_JWT_ACCESS_SECRET',process.env.AUTH_JWT_ACCESS_SECRET)
-      const decoded = verify(token, process.env.AUTH_JWT_ACCESS_SECRET);
-      console.log('dec',decoded)
+      const parts = token.split('.');
+      if (parts.length !== 3) {
+        throw new UnauthorizedException('Invalid token format');
+      }
 
-      return decoded; // Return user type from decoded payload
+      const decodedHeader = atob(parts[0]); // Decode header (optional)
+      const decodedPayload = atob(parts[1]); // Decode payload
+
+      // Parse the decoded payload into a JavaScript object (assuming it's JSON)
+      const payload = JSON.parse(decodedPayload);
+
+      // Return the decoded payload containing user information
+      return payload;
     } catch (error) {
       throw new UnauthorizedException('Invalid token or unauthorized access');
     }
