fde27a6c50b4604bc667dbd4891b40c6ae0adab8
partovi <partovi74@gmail.com>
2024-07-01 09:52:17 +0330

    add favorites

--- src/base/storage/file/app-file.controller.ts
+++ src/base/storage/file/app-file.controller.ts
@@ -1,14 +1,26 @@
-import { Controller, Get, Res } from "@nestjs/common";
+import {  Controller, Get, Res } from "@nestjs/common";
 import { Response } from "express";
 import { Public } from "src/users/auth/decorators/public.decorator";
-
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { Inject } from "@nestjs/common";
+import { Cache } from "cache-manager";
+import { CacheTTL } from "src/base/utilities/cache-ttl.util";
 @Controller("app/version")
 export class AppFileController {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
   @Public()
   @Get()
   async getVersion(@Res() res: Response) {
-    // Retrieve the version from the environment variable
+    const cacheKey = `app_version`;
+    const cachedData = await this.cacheManager.get<string>(
+      cacheKey,
+    );
+
+    if (cachedData) {
+      res.send(cachedData);
+    }
     const appVersion = process.env.APP_VERSION || "1.0.0";
+    await this.cacheManager.set(cacheKey, appVersion,CacheTTL.ONE_DAY);
     res.send(appVersion);
   }
 }
