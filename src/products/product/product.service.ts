7fde89a52f5c29e7216020b7c3412931d4b46e04
partovi <partovi74@gmail.com>
2024-07-07 12:29:40 +0330

    add slider url + find one brand

--- src/products/product/product.service.ts
+++ src/products/product/product.service.ts
@@ -895,49 +895,39 @@ export class ProductService {
   }
   async getLowestPriceOf(product: Product): Promise<Price> {
     try {
-      const cacheKey = `product_${product.id}_lowestPrice`;
-  
-      const cachedResult = await this.cacheManager.get<string>(cacheKey);
-      if (cachedResult) {
-          const decompressedData = zlib.gunzipSync(Buffer.from(cachedResult, 'base64')).toString('utf-8');
-          const parsedData = JSON.parse(decompressedData);
-          if (parsedData) {
-              parsedData.createdAt = new Date(parsedData.createdAt);
-          }
-          // return parsedData;
+    const cacheKey = `product_${product.id}_lowestPrice`;
+
+    const cachedResult = await this.cacheManager.get<string>(cacheKey);
+    if (cachedResult) {
+      const decompressedData = zlib.gunzipSync(Buffer.from(cachedResult, 'base64')).toString('utf-8');
+      const parsedData: Price = JSON.parse(decompressedData);
+      if (parsedData) {
+        parsedData.createdAt = new Date(parsedData.createdAt);
       }
-  
+      // return parsedData;
+    }
       const IDS = product.id;
       const result = await Price.findOne({
-          where: { productId: IDS, deletedAt: IsNull() },
-          relations: ["seller", "seller.logoFile", "seller.contacts", "seller.contacts.country"],
-          order: {
-              createdAt: "DESC"
-          },
-          // select: [
-          //     "id", 
-          //     "type", 
-          //     "amount", 
-          //     "isPublic", 
-          //     "discount"
-          // ]
+        where: { productId: IDS, deletedAt: IsNull() },
+        relations: ["seller"],
+        order: {
+          createdAt: "DESC"
+        },
       });
-  
+
       if (result) {
         const jsonString = JSON.stringify(result).replace(/__seller__/g, 'seller')
         .replace(/__logoFile__/g, 'logoFile');
         const modifiedDataWithOutText = JSON.parse(jsonString);
         const compressedData = zlib.gzipSync(JSON.stringify(modifiedDataWithOutText));
-        await this.cacheManager.set(cacheKey, compressedData, CacheTTL.TWO_WEEK);
+        await this.cacheManager.set(cacheKey, compressedData, CacheTTL.ONE_DAY);
       }
-      console.log('result',result)
-  
-      return result || null;
-  
-  } catch (e) {
-      console.log('eeeeeeeeeeee', e);
-  }
-  
+      console.log('res',result)
+      return result || null
+      
+    } catch (e) {
+      console.log('eeeeeeeeeeee',e)
+    }
     
   }
 
