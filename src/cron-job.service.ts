0dd3ee58ca40d4d5f811be4c359440d904789fb0
partovi <partovi74@gmail.com>
2024-07-09 11:06:06 +0330

    add nginx

--- src/cron-job.service.ts
+++ src/cron-job.service.ts
@@ -35,35 +35,35 @@ export class CronJobService {
   //   await this.fetchAndLogSellerViewsToElasticsearch();
   // }
 
-  @Cron(CronExpression.EVERY_5_SECONDS)
-  async logTrackerViewsToElasticsearch() {
-    const allKeys: string[] = await this.cacheManager.store.keys();
-    const productKeys: string[] = allKeys.filter(key =>
-      key.startsWith("eventTracker:"),
-    );
+  // @Cron(CronExpression.EVERY_5_SECONDS)
+  // async logTrackerViewsToElasticsearch() {
+  //   const allKeys: string[] = await this.cacheManager.store.keys();
+  //   const productKeys: string[] = allKeys.filter(key =>
+  //     key.startsWith("eventTracker:"),
+  //   );
 
-    const views: any[] = await Promise.all(
-      productKeys.map(async key => {
-        const value = await this.cacheManager.get(key);
-        this.cacheManager.del(key);
+  //   const views: any[] = await Promise.all(
+  //     productKeys.map(async key => {
+  //       const value = await this.cacheManager.get(key);
+  //       this.cacheManager.del(key);
 
-        return { key, value };
-      }),
-    );
-    for (const view of views) {
-      try {
+  //       return { key, value };
+  //     }),
+  //   );
+  //   for (const view of views) {
+  //     try {
        
       
-        const data : CreateEventTrackerInput = JSON.parse(view.value)
-        const event: EventTracker = EventTracker.create<EventTracker>(data);
-        await event.save()
+  //       const data : CreateEventTrackerInput = JSON.parse(view.value)
+  //       const event: EventTracker = EventTracker.create<EventTracker>(data);
+  //       await event.save()
 
-      } catch (error) {
-        // Handle error appropriately
-        console.error("Error logging view to Elasticsearch:", error.message);
-      }
-    }
-  }
+  //     } catch (error) {
+  //       // Handle error appropriately
+  //       console.error("Error logging view to Elasticsearch:", error.message);
+  //     }
+  //   }
+  // }
 
   // @Cron(CronExpression.EVERY_2_MINUTES)
   // async logCommand() {
@@ -108,71 +108,71 @@ export class CronJobService {
   //   }
   // }
 
-@Cron(CronExpression.EVERY_5_MINUTES)
-async logCommand() {
-  const randomTimeout = Math.floor(Math.random() * (15000 - 5000 + 1) + 5000);
+// @Cron(CronExpression.EVERY_5_MINUTES)
+// async logCommand() {
+//   const randomTimeout = Math.floor(Math.random() * (15000 - 5000 + 1) + 5000);
 
-  await new Promise(resolve => setTimeout(resolve, randomTimeout));
+//   await new Promise(resolve => setTimeout(resolve, randomTimeout));
 
-  const allKeys: string[] = await this.cacheManager.store.keys();
-  const productKeys: string[] = allKeys.filter(key =>
-    key.startsWith("pnpm"),
-  );
+//   const allKeys: string[] = await this.cacheManager.store.keys();
+//   const productKeys: string[] = allKeys.filter(key =>
+//     key.startsWith("pnpm"),
+//   );
 
-  const views: any[] = await Promise.all(
-    productKeys.map(async key => {
-      const value = await this.cacheManager.get(key);
-      this.cacheManager.del(key);
-      return { key, value };
-    }),
-  );
+//   const views: any[] = await Promise.all(
+//     productKeys.map(async key => {
+//       const value = await this.cacheManager.get(key);
+//       this.cacheManager.del(key);
+//       return { key, value };
+//     }),
+//   );
 
-  await this.processViewsSequentially(views);
-}
+//   await this.processViewsSequentially(views);
+// }
 
-private async processViewsSequentially(views: any[], index: number = 0) {
-  // Remove duplicates based on the 'key' property
-  const uniqueViews = views.filter((view, currentIndex) => {
-    const firstIndex = views.findIndex((v) => v.key === view.key);
-    return currentIndex === firstIndex;
-  });
+// private async processViewsSequentially(views: any[], index: number = 0) {
+//   // Remove duplicates based on the 'key' property
+//   const uniqueViews = views.filter((view, currentIndex) => {
+//     const firstIndex = views.findIndex((v) => v.key === view.key);
+//     return currentIndex === firstIndex;
+//   });
 
-  if (index < uniqueViews.length) {
-    const view = uniqueViews[index];
+//   if (index < uniqueViews.length) {
+//     const view = uniqueViews[index];
 
-    try {
-      await this.processView(view);
-    } catch (error) {
-      // Handle error appropriately
-      console.error("Error logging view to command:", error.message);
-    }
+//     try {
+//       await this.processView(view);
+//     } catch (error) {
+//       // Handle error appropriately
+//       console.error("Error logging view to command:", error.message);
+//     }
 
-    // Process the next view recursively
-    await this.processViewsSequentially(uniqueViews, index + 1);
-  }
-}
+//     // Process the next view recursively
+//     await this.processViewsSequentially(uniqueViews, index + 1);
+//   }
+// }
 
-private async processView(view: any) {
-  this.cacheManager.del(view.key);
+// private async processView(view: any) {
+//   this.cacheManager.del(view.key);
 
-  console.log('views', view);
+//   console.log('views', view);
 
-  const valueObject = JSON.parse(view.value);
-  const sellerId = `02-${valueObject.sellerId}`;
-  const files = File.findOneBy({ id: valueObject.file });
+//   const valueObject = JSON.parse(view.value);
+//   const sellerId = `02-${valueObject.sellerId}`;
+//   const files = File.findOneBy({ id: valueObject.file });
   
-  if (!files) {
-    throw 'not found';
-  }
+//   if (!files) {
+//     throw 'not found';
+//   }
 
-  const name = (await files).name;
-  const fileStream = await this.minioClient.getObject('vardast', name);
+//   const name = (await files).name;
+//   const fileStream = await this.minioClient.getObject('vardast', name);
 
-  const folderPath = '/usr/src/app';  // Replace with the actual folder path
-  const savedFilePath = await this.saveFileToLocalFolder(fileStream, sellerId, folderPath);
+//   const folderPath = '/usr/src/app';  // Replace with the actual folder path
+//   const savedFilePath = await this.saveFileToLocalFolder(fileStream, sellerId, folderPath);
 
-  await this.executePnpmCommand(sellerId);
-}
+//   await this.executePnpmCommand(sellerId);
+// }
 
 
   
@@ -203,192 +203,192 @@ private async processView(view: any) {
      
   // }
 
-  private async saveFileToLocalFolder(fileStream, fileName, baseFolderPath2) {
+  // private async saveFileToLocalFolder(fileStream, fileName, baseFolderPath2) {
 
-    const baseFolderPath = "/usr/src/app";
-    const filePath = path.join(baseFolderPath, fileName);
+  //   const baseFolderPath = "/usr/src/app";
+  //   const filePath = path.join(baseFolderPath, fileName);
 
-  try {
-    const writeStream = fs.createWriteStream(filePath);
-    fileStream.pipe(writeStream);
+  // try {
+  //   const writeStream = fs.createWriteStream(filePath);
+  //   fileStream.pipe(writeStream);
 
-    return new Promise((resolve, reject) => {
-      writeStream.on('finish', () => {
-        console.log(`File saved successfully at: ${filePath}`);
-        resolve(filePath);
-      });
+  //   return new Promise((resolve, reject) => {
+  //     writeStream.on('finish', () => {
+  //       console.log(`File saved successfully at: ${filePath}`);
+  //       resolve(filePath);
+  //     });
 
-      writeStream.on('error', (error) => {
-        console.error("Error saving file to local folder:", error.message);
-        reject(error);
-      });
-    });
-  } catch (error) {
-    console.error("Error saving file to local folder:", error.message);
-  }
-  }
-  private async fetchAndLogBrandViewsToElasticsearch(): Promise<void> {
-    const allKeys: string[] = await this.cacheManager.store.keys();
-    const productKeys: string[] = allKeys.filter(key =>
-      key.startsWith("brand_views_"),
-    );
+  //     writeStream.on('error', (error) => {
+  //       console.error("Error saving file to local folder:", error.message);
+  //       reject(error);
+  //     });
+  //   });
+  // } catch (error) {
+  //   console.error("Error saving file to local folder:", error.message);
+  // }
+  // }
+  // private async fetchAndLogBrandViewsToElasticsearch(): Promise<void> {
+  //   const allKeys: string[] = await this.cacheManager.store.keys();
+  //   const productKeys: string[] = allKeys.filter(key =>
+  //     key.startsWith("brand_views_"),
+  //   );
 
-    const views: any[] = await Promise.all(
-      productKeys.map(async key => {
-        const value = await this.cacheManager.get(key);
+  //   const views: any[] = await Promise.all(
+  //     productKeys.map(async key => {
+  //       const value = await this.cacheManager.get(key);
 
-        return { key, value };
-      }),
-    );
+  //       return { key, value };
+  //     }),
+  //   );
 
-    // await this.logBrandToElasticsearch(views);
-  }
+  //   // await this.logBrandToElasticsearch(views);
+  // }
 
-  private async executePnpmCommand(filePath) {
-    // Implement your pnpm command execution logic here
-    // Example:
-    const { exec } = require('child_process');
-    const command = `pnpm a seller:price "${filePath}"`;
-    const child = exec(command);
+  // private async executePnpmCommand(filePath) {
+  //   // Implement your pnpm command execution logic here
+  //   // Example:
+  //   const { exec } = require('child_process');
+  //   const command = `pnpm a seller:price "${filePath}"`;
+  //   const child = exec(command);
 
   
-    child.stdout.on('data', (data) => {
-      console.log(`pnpm command output: ${data}`);
-    });
+  //   child.stdout.on('data', (data) => {
+  //     console.log(`pnpm command output: ${data}`);
+  //   });
   
-    child.stderr.on('data', (data) => {
-      console.error(`pnpm command errors: ${data}`);
-    });
+  //   child.stderr.on('data', (data) => {
+  //     console.error(`pnpm command errors: ${data}`);
+  //   });
   
-    await new Promise((resolve) => {
-      child.on('exit', resolve);
-    });
-  }
-  private async fetchAndLogSellerViewsToElasticsearch(): Promise<void> {
-    const allKeys: string[] = await this.cacheManager.store.keys();
-    const productKeys: string[] = allKeys.filter(key =>
-      key.startsWith("seller_views_"),
-    );
+  //   await new Promise((resolve) => {
+  //     child.on('exit', resolve);
+  //   });
+  // }
+  // private async fetchAndLogSellerViewsToElasticsearch(): Promise<void> {
+  //   const allKeys: string[] = await this.cacheManager.store.keys();
+  //   const productKeys: string[] = allKeys.filter(key =>
+  //     key.startsWith("seller_views_"),
+  //   );
 
-    const views: any[] = await Promise.all(
-      productKeys.map(async key => {
-        const value = await this.cacheManager.get(key);
+  //   const views: any[] = await Promise.all(
+  //     productKeys.map(async key => {
+  //       const value = await this.cacheManager.get(key);
 
-        return { key, value };
-      }),
-    );
+  //       return { key, value };
+  //     }),
+  //   );
 
-    await this.logSellerToElasticsearch(views);
-  }
-  private async fetchAndLogProductViewsToElasticsearch(): Promise<void> {
-    const allKeys: string[] = await this.cacheManager.store.keys();
-    const productKeys: string[] = allKeys.filter(key =>
-      key.startsWith("product_views_"),
-    );
+  //   await this.logSellerToElasticsearch(views);
+  // }
+  // private async fetchAndLogProductViewsToElasticsearch(): Promise<void> {
+  //   const allKeys: string[] = await this.cacheManager.store.keys();
+  //   const productKeys: string[] = allKeys.filter(key =>
+  //     key.startsWith("product_views_"),
+  //   );
 
-    const views: any[] = await Promise.all(
-      productKeys.map(async key => {
-        const value = await this.cacheManager.get(key);
+  //   const views: any[] = await Promise.all(
+  //     productKeys.map(async key => {
+  //       const value = await this.cacheManager.get(key);
 
-        return { key, value };
-      }),
-    );
+  //       return { key, value };
+  //     }),
+  //   );
 
-    await this.logViewsToElasticsearch(views);
-  }
+  //   await this.logViewsToElasticsearch(views);
+  // }
 
-  private async fetchRequestViewsToElasticsearch(): Promise<void> {
-    const allKeys: string[] = await this.cacheManager.store.keys();
-    const productKeys: string[] = allKeys.filter(key =>
-      key.startsWith("request_"),
-    );
+  // private async fetchRequestViewsToElasticsearch(): Promise<void> {
+  //   const allKeys: string[] = await this.cacheManager.store.keys();
+  //   const productKeys: string[] = allKeys.filter(key =>
+  //     key.startsWith("request_"),
+  //   );
 
-    const views: any[] = await Promise.all(
-      productKeys.map(async key => {
-        const value = await this.cacheManager.get(key);
+  //   const views: any[] = await Promise.all(
+  //     productKeys.map(async key => {
+  //       const value = await this.cacheManager.get(key);
 
-        return { key, value };
-      }),
-    );
+  //       return { key, value };
+  //     }),
+  //   );
 
-    await this.logViewsToElasticsearch(views);
-  }
+  //   await this.logViewsToElasticsearch(views);
+  // }
 
-  private async logViewsToElasticsearch(views: any[]): Promise<void> {
-    const elasticsearchUrl = "http://elasticsearch:9200";
-    const indexName = "product_views";
-    if (views.length == 0) {
-      return;
-    }
-    for (const view of views) {
-      try {
-        this.cacheManager.del(view.key);
-        await axios
-          .post(`${elasticsearchUrl}/${indexName}/_doc`, view)
-          .then(e => {})
-          .catch(e => {});
-      } catch (error) {
-        // Handle error appropriately
-        console.error("Error logging view to Elasticsearch:", error.message);
-      }
-    }
-  }
-  private async logSellerToElasticsearch(views: any[]): Promise<void> {
-    const elasticsearchUrl = "http://elasticsearch:9200";
-    const indexName = "seller_views";
-    if (views.length == 0) {
-      return;
-    }
-    for (const view of views) {
-      try {
-        this.cacheManager.del(view.key);
-        await axios
-          .post(`${elasticsearchUrl}/${indexName}/_doc`, view)
-          .then(e => {})
-          .catch(e => {});
-      } catch (error) {
-        // Handle error appropriately
-        console.error("Error logging view to Elasticsearch:", error.message);
-      }
-    }
-  }
-  private async logBrandToElasticsearch(views: any[]): Promise<void> {
-    const elasticsearchUrl = "http://elasticsearch:9200";
-    const indexName = "brand_views";
-    if (views.length == 0) {
-      return;
-    }
-    for (const view of views) {
-      try {
-        this.cacheManager.del(view.key);
-        await axios
-          .post(`${elasticsearchUrl}/${indexName}/_doc`, view)
-          .then(e => {})
-          .catch(e => {});
-      } catch (error) {
-        // Handle error appropriately
-        console.error("Error logging view to Elasticsearch:", error.message);
-      }
-    }
-  }
+  // private async logViewsToElasticsearch(views: any[]): Promise<void> {
+  //   const elasticsearchUrl = "http://elasticsearch:9200";
+  //   const indexName = "product_views";
+  //   if (views.length == 0) {
+  //     return;
+  //   }
+  //   for (const view of views) {
+  //     try {
+  //       this.cacheManager.del(view.key);
+  //       await axios
+  //         .post(`${elasticsearchUrl}/${indexName}/_doc`, view)
+  //         .then(e => {})
+  //         .catch(e => {});
+  //     } catch (error) {
+  //       // Handle error appropriately
+  //       console.error("Error logging view to Elasticsearch:", error.message);
+  //     }
+  //   }
+  // }
+  // private async logSellerToElasticsearch(views: any[]): Promise<void> {
+  //   const elasticsearchUrl = "http://elasticsearch:9200";
+  //   const indexName = "seller_views";
+  //   if (views.length == 0) {
+  //     return;
+  //   }
+  //   for (const view of views) {
+  //     try {
+  //       this.cacheManager.del(view.key);
+  //       await axios
+  //         .post(`${elasticsearchUrl}/${indexName}/_doc`, view)
+  //         .then(e => {})
+  //         .catch(e => {});
+  //     } catch (error) {
+  //       // Handle error appropriately
+  //       console.error("Error logging view to Elasticsearch:", error.message);
+  //     }
+  //   }
+  // }
+  // private async logBrandToElasticsearch(views: any[]): Promise<void> {
+  //   const elasticsearchUrl = "http://elasticsearch:9200";
+  //   const indexName = "brand_views";
+  //   if (views.length == 0) {
+  //     return;
+  //   }
+  //   for (const view of views) {
+  //     try {
+  //       this.cacheManager.del(view.key);
+  //       await axios
+  //         .post(`${elasticsearchUrl}/${indexName}/_doc`, view)
+  //         .then(e => {})
+  //         .catch(e => {});
+  //     } catch (error) {
+  //       // Handle error appropriately
+  //       console.error("Error logging view to Elasticsearch:", error.message);
+  //     }
+  //   }
+  // }
 
-  private async logTrackerToElasticsearch(views: any[]): Promise<void> {
-    const elasticsearchUrl = "http://elasticsearch:9200";
-    const indexName = "track_contact";
-    if (views.length == 0) {
-      return;
-    }
-    for (const view of views) {
-      try {
-        this.cacheManager.del(view.key);
-        await axios
-          .post(`${elasticsearchUrl}/${indexName}/_doc`, view)
-          .then(e => {})
-          .catch(e => {});
-      } catch (error) {
-        // Handle error appropriately
-        console.error("Error logging view to Elasticsearch:", error.message);
-      }
-    }
-  }
+  // private async logTrackerToElasticsearch(views: any[]): Promise<void> {
+  //   const elasticsearchUrl = "http://elasticsearch:9200";
+  //   const indexName = "track_contact";
+  //   if (views.length == 0) {
+  //     return;
+  //   }
+  //   for (const view of views) {
+  //     try {
+  //       this.cacheManager.del(view.key);
+  //       await axios
+  //         .post(`${elasticsearchUrl}/${indexName}/_doc`, view)
+  //         .then(e => {})
+  //         .catch(e => {});
+  //     } catch (error) {
+  //       // Handle error appropriately
+  //       console.error("Error logging view to Elasticsearch:", error.message);
+  //     }
+  //   }
+  // }
 }
