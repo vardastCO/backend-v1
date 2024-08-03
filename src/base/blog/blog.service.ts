f9b805d8e7ceb77618270f16495344fae206b57c
partovi <partovi74@gmail.com>
2024-08-03 15:04:42 +0330

    change line dto in orderlines

--- src/base/blog/blog.service.ts
+++ src/base/blog/blog.service.ts
@@ -21,92 +21,85 @@ export class BlogService {
     indexBlogInput?: IndexBlogInput,
   ): Promise<PaginationBlogResponse> {
     try {
-      // const cacheKey = `blogs_${JSON.stringify(indexBlogInput)}`;
-      // const cachedData = await this.cacheManager.get<string>(
-      //   cacheKey,
-      // );
-      // let posts 
-      // if (cachedData) {
-      //   const decompressedData = zlib.gunzipSync(Buffer.from(cachedData, 'base64')).toString('utf-8');
-      //   const parsedData: PaginationBlogResponse = JSON.parse(decompressedData);
-      //   return parsedData;
-      // }
-      // if (indexBlogInput.categoryId) {
-      //   const categoriesParam = this.getCategoryMapping(indexBlogInput.categoryId);
-      //   const [response_1] = await Promise.all([
-      //     axios.get(`https://blog.vardast.com/wp-json/wp/v2/posts?per_page=5&_embed&categories=${categoriesParam}`),
-      //   ]);
+      const cacheKey = `blogs_${JSON.stringify(indexBlogInput)}`;
+      const cachedData = await this.cacheManager.get<string>(
+        cacheKey,
+      );
+      let posts 
+      if (cachedData) {
+        const decompressedData = zlib.gunzipSync(Buffer.from(cachedData, 'base64')).toString('utf-8');
+        const parsedData: PaginationBlogResponse = JSON.parse(decompressedData);
+        return parsedData;
+      }
+      if (indexBlogInput.categoryId) {
+        const categoriesParam = this.getCategoryMapping(indexBlogInput.categoryId);
+        const [response_1] = await Promise.all([
+          axios.get(`https://blog.vardast.com/wp-json/wp/v2/posts?per_page=5&_embed&categories=${categoriesParam}`),
+        ]);
     
-      //   posts = [
-      //     ...(response_1.data?.slice(0, 5) || []),
-      //   ];
-      // } else {
-      //   const [response_1, response_2, response_3] = await Promise.all([
-      //     axios.get('https://blog.vardast.com/wp-json/wp/v2/posts?per_page=1&_embed&categories=4'),
-      //     axios.get('https://blog.vardast.com/wp-json/wp/v2/posts?per_page=3&_embed&categories=24'),
-      //     axios.get('https://blog.vardast.com/wp-json/wp/v2/posts?per_page=1&_embed&categories=5')
-      //   ]);
+        posts = [
+          ...(response_1.data?.slice(0, 5) || []),
+        ];
+      } else {
+        const [response_1, response_2, response_3] = await Promise.all([
+          axios.get('https://blog.vardast.com/wp-json/wp/v2/posts?per_page=1&_embed&categories=4'),
+          axios.get('https://blog.vardast.com/wp-json/wp/v2/posts?per_page=3&_embed&categories=24'),
+          axios.get('https://blog.vardast.com/wp-json/wp/v2/posts?per_page=1&_embed&categories=5')
+        ]);
     
-      //   posts = [
-      //     ...(response_1.data?.slice(0, 1) || []),
-      //     ...(response_2.data?.slice(0, 3) || []),
-      //     ...(response_3.data?.slice(0, 1) || [])
-      //   ];
-      // }
+        posts = [
+          ...(response_1.data?.slice(0, 1) || []),
+          ...(response_2.data?.slice(0, 3) || []),
+          ...(response_3.data?.slice(0, 1) || [])
+        ];
+      }
 
-      // const sortedPosts = posts.sort((a, b) => {
-      //   const modifiedDataA = new Date(a.date);
-      //   const modifiedDataB = new Date(b.date);
-      //   if (modifiedDataA > modifiedDataB) {
-      //     return -1;
-      //   }
-      //   if (modifiedDataA < modifiedDataB) {
-      //     return 1;
-      //   }
-      //   return 0;
-      // })
+      const sortedPosts = posts.sort((a, b) => {
+        const modifiedDataA = new Date(a.date);
+        const modifiedDataB = new Date(b.date);
+        if (modifiedDataA > modifiedDataB) {
+          return -1;
+        }
+        if (modifiedDataA < modifiedDataB) {
+          return 1;
+        }
+        return 0;
+      })
 
-      // const createdBlogs: Blog[] = await Promise.all(
-      //   sortedPosts.map(async post => {
-      //     const dataUrl = post.link;
-      //     const date = post.date;
-      //     const title = post.title.rendered;
-      //     const description = post.excerpt.rendered;
-      //     const categoryId = 1;
-      //     const image_url =
-      //       post?._embedded["wp:featuredmedia"]?.[0]?.media_details?.sizes
-      //         ?.medium_large?.source_url || "";
-      //     const newBlog = await this.createBlog(
-      //       dataUrl,
-      //       title,
-      //       image_url,
-      //       description,
-      //       categoryId,
-      //       date
-      //     );
+      const createdBlogs: Blog[] = await Promise.all(
+        sortedPosts.map(async post => {
+          const dataUrl = post.link;
+          const date = post.date;
+          const title = post.title.rendered;
+          const description = post.excerpt.rendered;
+          const categoryId = 1;
+          const image_url =
+            post?._embedded["wp:featuredmedia"]?.[0]?.media_details?.sizes
+              ?.medium_large?.source_url || "";
+          const newBlog = await this.createBlog(
+            dataUrl,
+            title,
+            image_url,
+            description,
+            categoryId,
+            date
+          );
 
-      //     return newBlog;
-      //   }),
-      // );
+          return newBlog;
+        }),
+      );
   
-      // const paginatedBlogs = createdBlogs.slice(0, 5);
+      const paginatedBlogs = createdBlogs.slice(0, 5);
 
-      // const result: PaginationBlogResponse = PaginationBlogResponse.make(
-      //   indexBlogInput,
-      //   posts.length,
-      //   paginatedBlogs,
-      // );
-      
-      
       const result: PaginationBlogResponse = PaginationBlogResponse.make(
         indexBlogInput,
-        0,
-        [],
+        posts.length,
+        paginatedBlogs,
       );
       
-      // const compressedData = zlib.gzipSync(JSON.stringify(result));
+      const compressedData = zlib.gzipSync(JSON.stringify(result));
 
-      // await this.cacheManager.set(cacheKey, compressedData,CacheTTL.TWELVE_HOURS);
+      await this.cacheManager.set(cacheKey, compressedData,CacheTTL.ONE_DAY);
 
       return result;
     } catch (e) {
